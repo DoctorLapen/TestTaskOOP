f6bccd86a0c87893b0be76647646866f91305d0b
DoctorLapen <kostia1272@gmail.com>
2019-06-01 16:59:34 +0300

    add .gitighore attempt 8

---
+++ TestTaskOOP/Shapes/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskOOP
+{ /*
+    Абстрактный клас Shape описывает общие методы для всех фигур.
+    Он является предком для всех классов фигур
+  */
+    public abstract class Shape
+    {  /*
+         Поля доступны толко для чтения,
+         потому что данные в класах фигур только инициализируеться в конструкторе клаcса,а потом не изменяються
+         более простой аналог геттеров
+        */
+
+        public readonly string color;
+
+        
+
+        /* конструктор определен в абстрактном классе
+         * для того что бы каждый раз вручную не инициализировать поле color в класcах потомках
+         * а передавать значение через base
+         */
+        public Shape(string color) {
+            this.color = color;
+        }
+     
+      //Абстрактный метод draw выводить информацию об объекте в консоль.
+       public  abstract void draw();
+        
+      //Абстрактный метод getArea возвращает площадь геометрической фигуры.
+       
+        public abstract float getArea();
+
+       
+
+        
+    }
+}
---
+++ TestTaskOOP/Shapes/Square.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskOOP
+{
+    class Square : Shape
+    {
+        /*
+         Поля доступны толко для чтения,
+         потому что данные в класах фигур только инициализируеться в конструкторе клаcса,а потом не изменяються
+         более простой аналог геттеров
+        */
+        public readonly float side;
+
+        public Square(string color,float side):base(color)
+        {
+            this.side = side;
+        }
+
+        //Реализация абстрактного метода draw  который выводить информацию об объекте в консоль.
+        public override void draw()
+        {
+            Console.WriteLine("Shape : Square color : {0}   side : {1:f2} area : {2:f2}", color,side,getArea());
+        }
+       
+        /*
+        Реализация абстрактного метода getArea который возвращает площадь квадрата.
+        Без использования  метода из Math код становиться более читабельным
+       */
+        public override float getArea()
+        {
+            return side * side;
+        }
+
+
+
+    }
+}
